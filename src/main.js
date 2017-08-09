d6b7ce86f86b4a833aa324d5fe2cf15961eae028
Emre-Kul <emrekul35@gmail.com>
2017-08-09 16:29:41 +0300

    React initialize

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(
+    <h1>This is React</h1>,
+    document.getElementById('root')
+  );
+});

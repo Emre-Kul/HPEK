94e1377c445168bbdb707851fac0074b0a288933
Emre-Kul <emrekul35@gmail.com>
2017-08-25 14:23:53 +0300

    detail tip menu rating bug fixed

--- src/detail/component/detail-image-card/DetailImageCard.jsx
+++ src/detail/component/detail-image-card/DetailImageCard.jsx
@@ -53,7 +53,7 @@ export class DetailImageCard extends Component{
               <div className="detail-image-card-user-info-container">
                 <div className="detail-image-card-user-info-image-container">
                   <img className="detail-image-card-user-info-image"
-                       src={`${this.props.userInfo.photo.prefix}60x60${this.props.userInfo.photo.suffix}`} />
+                       src={`${this.props.userInfo.photo.prefix}60x60${this.props.userInfo.photo.suffix}`}/>
                 </div>
                 {this.props.userInfo.firstName} {this.props.userInfo.lastName}
               </div>

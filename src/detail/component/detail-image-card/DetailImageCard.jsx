6c215d0aebbd9ccc1ec24ea35f1c378d701b9863
Emre-Kul <emrekul35@gmail.com>
2017-08-25 14:33:52 +0300

    image card mouse over javascript code changed to css hover

--- src/detail/component/detail-image-card/DetailImageCard.jsx
+++ src/detail/component/detail-image-card/DetailImageCard.jsx
@@ -9,58 +9,23 @@ export class DetailImageCard extends Component{
     backgroundImage : PropTypes.string,
     userInfo : PropTypes.object
   }
-
-  constructor (){
-    super();
-    this.state = {
-      showUserInfo : false
-    }
-
-    this.handleShowUserInfo = this.handleShowUserInfo.bind(this);
-    this.handleHideUserInfo = this.handleHideUserInfo.bind(this);
-
-  }
-
-  handleShowUserInfo(){
-    if(!this.state.showUserInfo){
-      this.setState({
-        showUserInfo : true
-      });
-    }
-  }
-  handleHideUserInfo(){
-    if(this.state.showUserInfo){
-      this.setState({
-        showUserInfo : false
-      });
-    }
-  }
-
+  
   render() {
     let backgroundStyle = {
       "backgroundImage" : `url(${this.props.backgroundImage})`
     }
     return (
       <div className="detail-image-card"
-           style={backgroundStyle}
-           onMouseEnter={this.handleShowUserInfo}
-           onMouseLeave={this.handleHideUserInfo}>
-
-        {
-          (this.state.showUserInfo) ?
-            <div className="detail-image-card-user-container">
-              <div className="detail-image-card-user-background"/>
+           style={backgroundStyle}>
               <div className="detail-image-card-user-info-container">
+                <div className="detail-image-card-user-info-content-container">
                 <div className="detail-image-card-user-info-image-container">
                   <img className="detail-image-card-user-info-image"
                        src={`${this.props.userInfo.photo.prefix}60x60${this.props.userInfo.photo.suffix}`}/>
                 </div>
                 {this.props.userInfo.firstName} {this.props.userInfo.lastName}
+                </div>
               </div>
-            </div>:
-            null
-        }
-
       </div>
 
     );

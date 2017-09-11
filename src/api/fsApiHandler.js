6dca26dc805749ba00a087a26fb5fb802c2e47af
Emre-Kul <emrekul35@gmail.com>
2017-09-11 17:48:23 +0300

    typo fixed and fsApiConsts created

--- src/api/fsApiHandler.js
+++ src/api/fsApiHandler.js
@@ -1,7 +1,4 @@
-const FS_CLIENT_ID = "2YKSC0BQ5GFTP0FH4ESR1ZUFQJMM135XJ5SFI3WZZOT1H2OK";
-const FS_CLIENT_SECRET = "V4M5SUGP5ENHW5UCOMRPIUVZ4LTOWOEFRHFFUS0J3ENU1VMU";
-const FS_API_VERSION = "20180101";
-const FS_API_KEY = `client_id=${FS_CLIENT_ID}&client_secret=${FS_CLIENT_SECRET}&v=${FS_API_VERSION}`;
+import {FS_API_KEY} from './fsApiConsts.js';
 
 const makeRequest = function(method,url){
   return new Promise((resolve,reject) =>{

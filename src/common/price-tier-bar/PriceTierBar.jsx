1ed20f2e4bd7aed5b1d24f7ab748d158dd27e5c8
Emre-Kul <emrekul35@gmail.com>
2017-09-12 14:15:16 +0300

    createPriceTierBarc function created

--- src/common/price-tier-bar/PriceTierBar.jsx
+++ src/common/price-tier-bar/PriceTierBar.jsx
@@ -11,26 +11,24 @@ export class PriceTierBar extends Component {
     priceTier: PropTypes.number,
     venueId: PropTypes.string
   }
-
-  render () {
-    let priceTierBarColors = ['#c9c5ff', '#a59fee', '#8b81ff', '#685dea']
+  createPriceTierBars = (venueId, priceTier) => {
     let priceTierBars = []
-    let priceTier = (this.props.priceTier > 4)
-      ? 4
-      : this.props.priceTier
-
+    const priceTierBarColors = ['#c9c5ff', '#a59fee', '#8b81ff', '#685dea']
     for (let i = 0; i < priceTier; i++) {
       priceTierBars.push(<div key={`${this.props.venueId}-priceTierBar-${i}`}
                               className="price-tier-bar-child"
                               style={{'backgroundColor': priceTierBarColors[i]}}/>)
     }
+    return priceTierBars
+  }
 
+  render () {
     return (
       <div className="price-tier-bar">
         <img src={imgTagIcon}
              className="price-tier-bar-icon"/>
         <div className="price-tier-bar-container">
-          {priceTierBars}
+          {this.createPriceTierBars(this.props.venueId, this.props.priceTier)}
         </div>
 
       </div>

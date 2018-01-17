66cfd122e4d604af2f62c7a0b397664db69d939c
Emre-Kul <emrekul35@gmail.com>
2018-01-17 21:54:26 +0300

    small change

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     './main.jsx',
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/'
   },
